ac6d8262097ce575351e2e2bff12e0c0abb42986
Volodymyr Lysenko <lusiy321@gmail.com>
2022-11-16 18:44:56 +0200

    upp

--- PostController.js
+++ PostController.js
@@ -1,10 +1,10 @@
-import post from "./post.js";
+import Post from "./Post.js";
 
 class PostController {
   async create(req, res) {
     try {
       const { test, username, password } = req.body;
-      const posts = await post.create({ test, username, password });
+      const posts = await Post.create({ test, username, password });
       return res.status(200).json(posts);
     } catch (e) {
       res.status(500).json(e);
@@ -12,7 +12,7 @@ class PostController {
   }
   async getAll(req, res) {
     try {
-      const posts = await post.find();
+      const posts = await Post.find();
       return res.json(posts);
     } catch (e) {
       res.status(500).json(e);
@@ -21,23 +21,38 @@ class PostController {
   async getOne(req, res) {
     try {
       const { id } = req.params;
-      if (id) {
+      if (!id) {
         res.status(400).json({ message: "Id not found" });
       }
-      const posts = await post.findById(id);
+      const posts = await Post.findById(id);
       return res.json(posts);
     } catch (e) {
       res.status(500).json(e);
     }
   }
+
   async uppdate(req, res) {
     try {
+      const post = req.body;
+      if (!post._id) {
+        res.status(400).json({ message: "Id not found" });
+      }
+      const updatePost = await Post.findByIdAndUpdate(post._id, post, {
+        new: true,
+      });
+      return res.json(updatePost);
     } catch (e) {
       res.status(500).json(e);
     }
   }
   async delete(req, res) {
     try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "Id not found" });
+      }
+      const post = await Post.findByIdAndDelete(id);
+      return res.json(post);
     } catch (e) {
       res.status(500).json(e);
     }
--- post.js
+++ post.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const post = new mongoose.Schema({
+const Post = new mongoose.Schema({
   test: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
 });
 
-export default mongoose.model("post", post);
+export default mongoose.model("post", Post);
